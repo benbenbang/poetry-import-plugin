c4723d4a4429d0b8f874ddc3c3f0386556aae677
benbenbang <bn@bitbrew.dev>
2024-05-19 21:16:40 +0200

    refactor(wip): changes in tests/fixtures/pyproject.py tests/test_command.py tests/test_read_requirements.py tests/test_toml_at_root.py

--- tests/test_command.py
+++ tests/test_command.py
@@ -1,11 +1,12 @@
 # standard library
 from pathlib import Path
 from typing import TYPE_CHECKING
-from unittest.mock import MagicMock, mock_open, patch
+from unittest.mock import MagicMock, patch
 
 # pypi library
 import pytest
 from cleo.io.buffered_io import BufferedIO
+from cleo.io.inputs.string_input import StringInput
 
 # poetry-import library
 from poetry_import.command import ImportReqCommand
@@ -109,7 +110,7 @@ def test_lock_or_install_dependencies(
 
 
 @pytest.mark.unittests
-def test_integrate_dependencies(command, pyproject_toml, pyproject_toml_raw):
+def test_update_pyproject_toml(command: "ImportReqCommand", pyproject_toml, pyproject_toml_raw):
     mocked_file = Path(pyproject_toml)
     expect = (
         pyproject_toml_raw
@@ -120,40 +121,37 @@ Django = "3.0"
 """
     )
 
-    dependencies = ["flask==1.0", "Django==3.0"]
-    group = "dev"
-    command._integrate_dependencies(group, dependencies)
+    group_specs = {
+        "dev": [
+            {"name": "flask", "version": "1.0"},
+            {"name": "Django", "version": "3.0"},
+        ]
+    }
+
+    command.update_pyproject_toml(group_specs)
 
     # Ensure the file was written to (updated)
     assert mocked_file.read_text() == expect
 
 
 @pytest.mark.unittests
-def test_apply_constraints(command: "ImportReqCommand"):
-    dependencies = ["flask", "Django"]
-    constraints = {"flask": "==1.1"}
-    expected = ["flask==1.1", "Django"]
-    result = command._apply_constraints(dependencies, constraints)
+def test_parse_constraint_file(command: "ImportReqCommand", project: "Project"):
+    dependencies = [f"{project['constraints']}"]
+    expected = {"flask": "2.0", "django": "3.0", "pydantic-settings": "2.3"}
+    result = command._parse_constraints_specifications(dependencies)
     assert result == expected, "Constraints were not applied correctly"
 
 
 @pytest.mark.unittests
-def test_command_full_flow(command: "ImportReqCommand", mocker: "MockerFixture"):
-    mocker.patch("pathlib.Path.open", new_callable=mock_open, read_data="flask==1.0\nDjango==3.0")
+def test_command_format_tokens(
+    command: "ImportReqCommand",
+    project: "Project",
+    mocker: "MockerFixture",
+):
+    mocker.patch.object(command, "_io", BufferedIO(StringInput("lock -g dev req_a.txt")))
 
-    # Setup command options and arguments
-    command.argument = MagicMock(return_value=["requirements.txt"])
-    command.option = MagicMock(side_effect=lambda x: {"group": ["-g", "dev", "requirements.txt"]}.get(x, False))
+    excepted = {"dev": ["req_a.txt"]}
 
-    # Mock _parse_group_specifications to simplify command line parsing logic in the test
-    command._parse_group_specifications = MagicMock(return_value=({"dev": ["requirements.txt"]}, None))
-    command._parse_requirements_file = MagicMock(return_value=["flask==1.0", "Django==3.0"])
-    command._integrate_dependencies = MagicMock()
-    command._update_or_install_dependencies = MagicMock()
+    file_groups = command._fromat_tokens()
 
-    # Run the command handle
-    command.handle()
-
-    # Assertions to check if the dependencies were integrated correctly
-    command._integrate_dependencies.assert_called_once_with("dev", ["flask==1.0", "Django==3.0"])
-    command._update_or_install_dependencies.assert_called_once()
+    assert file_groups == excepted, "Failed to parse the command line input correctly"
